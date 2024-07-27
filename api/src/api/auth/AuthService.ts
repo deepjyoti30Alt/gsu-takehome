18214622165d5f6a09540fefee4de9e4521464f9
Deepjyoti Barman <deep.barman30@gmail.com>
2024-07-27 15:33:24 +0530

    Integrate winston properly into the app

--- api/src/api/auth/AuthService.ts
+++ api/src/api/auth/AuthService.ts
@@ -4,11 +4,13 @@ import { OrmRepository } from 'typeorm-typedi-extensions';
 
 import { User } from '../../database/models/User';
 import { UserRepository } from '../../database/services/UserService';
+import { Logger, LoggerInterface } from '../../decorators/Logger';
 
 @Service()
 export class AuthService {
 
     constructor(
+        @Logger(__filename) private log: LoggerInterface,
         @OrmRepository() private userRepository: UserRepository
     ) { }
 
@@ -16,6 +18,7 @@ export class AuthService {
         const authorization = req.header('authorization');
 
         if (authorization && authorization.split(' ')[0] === 'Basic') {
+            this.log.info("Credentials found, parsing them.")
             const decodedBase64 = Buffer.from(authorization.split(' ')[1], 'base64').toString('ascii');
             const userId = decodedBase64.split(':')[0];
             const password = decodedBase64.split(':')[1];
@@ -24,6 +27,7 @@ export class AuthService {
             }
         }
 
+        this.log.info('Credentials not provided, cannot authorize')
         return undefined;
     }
 
